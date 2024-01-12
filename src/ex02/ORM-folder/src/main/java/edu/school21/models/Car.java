cd621b739911ca2875663c0e7879f20ccd139157
Sparr Viola <sparrvio@ga-h5.msk.21-school.ru>
2024-01-12 22:36:38 +0300

    start ex02

--- src/ex02/ORM-folder/src/main/java/edu/school21/models/Car.java
+++ src/ex02/ORM-folder/src/main/java/edu/school21/models/Car.java
@@ -1,4 +1,21 @@
 package edu.school21.models;
 
+import edu.school21.annotations.OrmColumn;
+import edu.school21.annotations.OrmColumnId;
+import edu.school21.annotations.OrmEntity;
+
+import javax.persistence.GenerationType;
+
+@OrmEntity(table = "simple_car")
 public class Car {
+    @OrmColumnId(id = GenerationType.IDENTITY)
+    private Long id;
+    @OrmColumn(name = "model", length = 10)
+    private String model;
+
+    @OrmColumn(name = "color", length = 10)
+    private String color;
+
+    @OrmColumn(name = "price")
+    private Double price;
 }
