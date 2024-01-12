90d717297398eb02bf65df8e30edad5041c1ce68
Sparr Viola <sparrvio@ga-h5.msk.21-school.ru>
2024-01-12 22:15:56 +0300

    start ex02

---
+++ src/ex02/ORM-folder/src/main/java/edu/school21/annotations/OrmColumnId.java
@@ -0,0 +1,12 @@
+package edu.school21.annotations;
+import javax.persistence.GenerationType;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface OrmColumnId {
+    GenerationType id() default GenerationType.AUTO;
+}
---
+++ src/ex02/ORM-folder/src/main/java/edu/school21/models/User.java
@@ -0,0 +1,21 @@
+package edu.school21.models;
+
+import edu.school21.annotations.OrmColumn;
+import edu.school21.annotations.OrmColumnId;
+import edu.school21.annotations.OrmEntity;
+
+import javax.persistence.GenerationType;
+
+
+
+@OrmEntity(table = "simple_user")
+public class User {
+    @OrmColumnId(id = GenerationType.IDENTITY)
+    private Long id;
+    @OrmColumn(name = "first_name", length = 10)
+    private String firstName;
+    @OrmColumn(name = "last_name", length = 10)
+    private String lastName;
+    @OrmColumn(name = "age")
+    private Integer age;
+}
